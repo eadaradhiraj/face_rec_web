0b3e497c2b5464046357995a067ae4902e25b154
Dhiraj Eadara <dhiraj.eadara@gramener.com>
2018-10-11 12:50:59 +0530

    WIP: Calcualte distance to nearest name

--- static/js/face_detection.js
+++ static/js/face_detection.js
@@ -19,19 +19,25 @@ function track_image(tracker, canvas, context) {
     context.clearRect(0, 0, canvas.width, canvas.height);
     event.data.forEach(function (rect) {
       context.strokeStyle = '#a64ceb';
-      loc = acquired_names ? acquired_names.dims : acquired_names
-      x = loc ? loc[3] : rect.x
-      name = acquired_names ? acquired_names.name : 'not yet'
-      y = loc ? loc[0] : rect.y
-      w = loc ? loc[1] : rect.width
-      h = loc ? loc[2] : rect.height
       context.strokeRect(rect.x, rect.y, rect.width, rect.height);
       context.font = '11px Helvetica';
       context.fillStyle = "#fff";
-      context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
-      context.fillText(name, x , y );
-      context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
-      // context.fillText(rect.x + rect.width + 5, rect.y + 33);
+      if (acquired_names != undefined) {
+        if (acquired_names.length>1)
+          {console.log(acquired_names)}
+        var dists = acquired_names.map(function (n) {
+          return {
+            'name': n.name,
+            "dist": Math.sqrt(
+              Math.pow((n.dims[0] - rect.x), 2) + Math.pow((n.dims[1] - rect.y), 2)
+            )
+          }
+        })
+        var nearest = dists.reduce(function (l, e) {
+          return e.dist < l.dist ? e : l;
+        });
+        context.fillText(nearest.name, rect.x, rect.y);
+      }
 
       var snapshotContext = snapshotCanvas.getContext('2d');
       //draw image to canvas. scale to target dimensions
@@ -46,8 +52,7 @@ function track_image(tracker, canvas, context) {
           'data': dataURI
         },
         success: function (response) {
-          console.log("rect", rect, "response", response)
-          acquired_names = response[0]//.dims
+          acquired_names = response
         }
       });
 
